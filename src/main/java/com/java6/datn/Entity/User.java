09afc821e0752c55049167b92a192e5a171e1861
Minh Thơ <117341110+MinhThow@users.noreply.github.com>
2025-06-23 19:19:21 +0700

    Rest api : Product, Category , User

---
+++ src/main/java/com/java6/datn/Entity/User.java
@@ -0,0 +1,39 @@
+package com.java6.datn.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Lob
+    private String address;
+
+    @Column(length = 20)
+    private String role; // 'customer' or 'admin'
+
+    @Column(name = "CreatedAt")
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
