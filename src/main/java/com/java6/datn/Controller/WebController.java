365aaa5748ff55e47749414ea6fa96cc12ef89b3
Minh Thơ <117341110+MinhThow@users.noreply.github.com>
2025-06-25 08:24:58 +0700

    Login thành công

--- src/main/java/com/java6/datn/Controller/WebController.java
+++ src/main/java/com/java6/datn/Controller/WebController.java
@@ -9,18 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
     @GetMapping("/shop")
     public String home() {
-        return "shop.html";
+        return "shop";
     }
 
     @GetMapping("/about")
     public String aboutpage() {
+        return "about"; // about.html trong templates
+    }
+
+    @GetMapping("/home")
+    public String homepage() {
         return "index"; // about.html trong templates
     }
 
 
     @GetMapping("/login")
     public String showLogin() {
-        return "Login_Register.html"; // trả file login.html
+        return "Login_Register"; // trả file login.html
     }
 
 /// login thành công
