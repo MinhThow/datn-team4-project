f9f3d750477c2a65711d8b6bc86fe4e152f7b382
Minh Thơ <117341110+MinhThow@users.noreply.github.com>
2025-08-25 02:01:14 +0700

    lọc theo danh mục

--- src/main/java/com/java6/datn/Repository/ProductRepository.java
+++ src/main/java/com/java6/datn/Repository/ProductRepository.java
@@ -2,10 +2,19 @@ package com.java6.datn.Repository;
 
 import com.java6.datn.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    // Cách dùng query method theo quan hệ
+    List<Product> findByCategory_CategoryID(Integer categoryID);
 
+    // Hoặc dùng @Query để rõ ràng hơn
+    @Query("SELECT p FROM Product p WHERE p.category.categoryID = :categoryID")
+    List<Product> findProductsByCategory(@Param("categoryID") Integer categoryID);
 }
 
